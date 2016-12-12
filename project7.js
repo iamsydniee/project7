bbe76e6ae0134dcd27a533986165c8317d095058
iamsydniee <sydniemcdonald009@gmail.com>
2016-12-12 10:36:20 -0800

    part 1 & 2 done
    drawcircle n drawsquare

--- project7.js
+++ project7.js
@@ -11,12 +11,31 @@ function transformPoint(event) {
 }
 
 // Step 2: drawSquare and drawCircle functions
-function drawSquare(x, y, size, color) {
-  // square drawing code here
+function drawSquare(xpos, ypos, size, color) {
+  var newrect = document.createElementNS(namespace, "square")
+ newsquare.setAttribute("x", xpos)
+ newsquare.setAttribute("y", ypos)
+ newsquare.setAttribute("height", size)
+  newsquare.setAttribute("width", size)
+ newsquare.setAttribute("fill", color)
+ canvas.appendChild(newsquare)
+}
+
+function drawCircle(xpos, ypos, size, color) {
+ var newcircle = document.createElementNS(namespace, "circle")
+ newcircle.setAttribute("cx", xpos)
+ newcircle.setAttribute("cy", ypos)
+ newcircle.setAttribute("size", size)
+ newcircle.setAttribute("fill", color)
+  canvas.appendChild(newcircle)
 }
 
 // Step 3: Event listeners
 document.addEventListener("mousedown", function(e) {
   // what do you want to do when the user presses down
   // on the mouse button?
+})
+
+document.addEventListener("mousemove", function(e){
+
 })
