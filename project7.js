eff078c061eb8880a0e796460d1177beee27728d
iamsydniee <sydniemcdonald009@gmail.com>
2016-12-14 08:51:45 -0800

    finished basics
    finished steps 1-9 adding extra

--- project7.js
+++ project7.js
@@ -1,6 +1,6 @@
 var screen = document.getElementById("screen")
 var namespace = "http://www.w3.org/2000/svg"
-
+var drawing = false;
 // utility function
 function transformPoint(event) {
   var pt = screen.createSVGPoint()
@@ -12,30 +12,54 @@ function transformPoint(event) {
 
 // Step 2: drawSquare and drawCircle functions
 function drawSquare(xpos, ypos, size, color) {
-  var newrect = document.createElementNS(namespace, "square")
+  var newsquare = document.createElementNS(namespace, "rect")
  newsquare.setAttribute("x", xpos)
  newsquare.setAttribute("y", ypos)
  newsquare.setAttribute("height", size)
-  newsquare.setAttribute("width", size)
+ newsquare.setAttribute("width", size)
  newsquare.setAttribute("fill", color)
- canvas.appendChild(newsquare)
+ screen.appendChild(newsquare)
 }
 
 function drawCircle(xpos, ypos, size, color) {
  var newcircle = document.createElementNS(namespace, "circle")
  newcircle.setAttribute("cx", xpos)
  newcircle.setAttribute("cy", ypos)
- newcircle.setAttribute("size", size)
+ newcircle.setAttribute("r", size)
  newcircle.setAttribute("fill", color)
-  canvas.appendChild(newcircle)
+  screen.appendChild(newcircle)
 }
 
 // Step 3: Event listeners
-document.addEventListener("mousedown", function(e) {
+screen = document.getElementById("screen")
+document.addEventListener("mousedown", function(event) {
   // what do you want to do when the user presses down
   // on the mouse button?
+drawing = true;
+
 })
 
-document.addEventListener("mousemove", function(e){
+document.addEventListener("mouseup", function(event){
+ drawing = false;
+})
+//  drawing stuff//
+document.addEventListener("mousemove", function(event){
+if (drawing == true) {
+var pt = transformPoint(event)
+  var xpos = pt.x
+  var ypos = pt.y
+
+  var shapeSelect = document.getElementById("shapeSelect").value
+  var sizeSelect = document.getElementById("sizeSelect").value
+  var colorSelect = document.getElementById("colorSelect").value
+
+if(shapeSelect == "square"){
+  drawSquare(pt.x, pt.y, sizeSelect, colorSelect)
+}
+
+else {
+  drawCircle(pt.x, pt.y, sizeSelect, colorSelect)
+}
+}
 
 })
